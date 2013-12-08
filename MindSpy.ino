75d8dad95a189dc4133dece1f01678594e0d3d10
Peter Boraros <pborky@pborky.sk>
2013-12-08 06:16:59 +0100

    fix logger initialization

--- MindSpy.ino
+++ MindSpy.ino
@@ -5,21 +5,21 @@
 #include "Logging.h"
 #include "ADS1298.h"
 
-ADS1298  sensor;
+ADS1298  Sensor;
 
 void setup() {
   Serial.begin(115200);
-  Log.init(LOG_LEVEL_VERBOSE, Serial);
-  sensor.begin();
+  Log.init(LOG_LEVEL_VERBOSE, &Serial);
+  Sensor.begin();
   delay(1000);
   test();
 }
 
 void test() {
-  //sensor.setRegister(0x01, 0x11);
+  //Sensor.setRegister(0x01, 0x11);
   Log.debug("Registers:"CR);
   for (int i = 0; i < 5; i++) {
-    byte r = sensor.getRegister(i);
+    byte r = Sensor.getRegister(i);
     Log.debug("%X -> %d"CR, i, r);
   }
 }
